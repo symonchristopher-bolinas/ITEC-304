0fc8e88650e8c9a963fc2c80120dc451a67eec60
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-10-10 07:12:41 -0700

    Create script.js

---
+++ Activity5/static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    document.getElementById("title").addEventListener("click", myFunction);
+
+    function myFunction() {
+    document.getElementById("title").innerHTML = "You Clicked!";
+    }
+
+});
