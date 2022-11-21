991dfd13728ab753f5c6eeec3edd3e8f59c557c5
symonchristopher-bolinas <101610568+symonchristopher-bolinas@users.noreply.github.com>
2022-11-21 05:34:06 -0800

    Create main.py

---
+++ Activity6/Activity6.2/main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+root = Builder.load_string('''
+FloatLayout:
+    canvas.before:
+        Color:
+            rgba: 0, 0, 255, 0.3
+        Rectangle:
+        # self here refers to the widget i.e FloatLayout
+            pos: self.pos
+            size: self.size
+    Button:
+        text: 'Hello World!!'
+        size_hint: .5, .5
+
+        pos_hint: {'center_x':.5, 'center_y': .5}
+''')
+class MainApp(App):
+    def build(self):
+        return root
+if __name__ == '__main__':
+    MainApp().run()
